7e042ba753b7399bf7651578da5098d5267125c0
paphonb <paphonb@gmail.com>
2019-09-19 23:14:18 +0700

    Fix launcher preview rendering
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Change-Id: I8c6fc263a3021f3ff929bc75b2730962b29d3758

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -107,6 +107,14 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         }
     }
 
+    private Launcher tryGetLauncher(Context context) {
+        try {
+            return Launcher.getLauncher(context);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
